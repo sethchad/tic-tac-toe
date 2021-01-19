158ab28693f6472ce0b346450fcc637bf474e9a7
Chadwick Seth W <seth.chadwick@gmail.com>
2021-01-19 10:04:22 -0600

    squares change to red or blue on alternating clicks and console logs a game over message after 9 turns

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', clickHandler));
 
+let turn = 0;
 
 function clickHandler(e) {
     e.preventDefault();
     console.log(e.target);
+
+    turn++;
+
+    if (turn < 10) {
+        if (turn % 2) {
+            e.target.style.backgroundColor = "blue";
+        }
+        else {
+            e.target.style.backgroundColor = "red";
+        }
+    } else {
+        console.log("game over")
+    }
 }
